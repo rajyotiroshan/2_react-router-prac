8296dc4a874c47b7f07840b0a96c7ab8aa3fcd9d
rajan <rajyotiroshan@gmail.com>
2023-04-11 21:43:46 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
-  return <h1>React router Practice Project</h1>;
+  return (
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
