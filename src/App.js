fad89db8b3cc98043119bbc88208e61dd6e5c0a7
rajan <rajyotiroshan@gmail.com>
2023-04-14 10:10:18 +0530

    final v1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
 
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 function Home(props) {
+  console.log(props);
   return (
     <div>
       <h1>Home Components</h1>
